c389e0b9ce23a95bd73e35bf8a5e53f3091c6510
phenomenal020 <stephenkalu2019@outlook.com>
2023-09-09 20:02:35 +0100

    Applied animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import Admission from "@/components/admission/Admission";
 import Testimonials from "@/components/testimonials/Testimonials";
 import CalendarAlt from "@/components/calendar/CalendarAlt";
 import BannerAlt from "@/components/banner/BannerAlt";
+import BannerTest from "@/components/banner/BannerTest";
 
 type Props = {};
 
@@ -17,6 +18,7 @@ function page({}: Props) {
     <div className={styles.container}>
       {/* <Banner /> */}
       <BannerAlt />
+      {/* <BannerTest /> */}
       <About />
       <Glance />
       <Admission />
--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import React, { useRef, useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
 import Image from "next/image";
 import styles from "./testimonials.module.css";
-import { useEffect, useState } from "react";
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 // import { collection, getDocs } from "firebase/firestore";
@@ -10,120 +19,157 @@ import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 type Props = {};
 
-interface imageIndices {
-  first: number;
-  second: number;
-  third: number;
+interface testimonial {
+  left: string;
+  main: string;
+  right: string;
+  comment: string;
+  role: string;
+  name: string;
 }
 
 const Testimonials = (props: Props) => {
-  const images: string[] = [
-    "/images/testimonial001.jpeg",
-    "/images/testimonial002.jpg",
-    "/images/testimonial003.jpeg",
-    "/images/testimonial004.jpeg",
-    "/images/testimonial005.jpeg",
-    "/images/testimonial006.jpeg",
+  /* Custom styles for Swiper navigation buttons */
+
+  const testimonials: testimonial[] = [
+    {
+      left: "/images/testimonial001.jpeg",
+      main: "/images/testimonial002.jpg",
+      right: "/images/testimonial003.jpeg",
+      comment:
+        "The teachers at this school are amazing! They truly care about each student's success. The teachers at this school are amazing! They truly care about each student's success. The teachers at this school are amazing! They truly care about each student's success.",
+      role: "Mathematics teacher",
+      name: "Amarachi Ezenwaka",
+    },
+    {
+      left: "/images/testimonial002.jpg",
+      main: "/images/testimonial003.jpeg",
+      right: "/images/testimonial004.jpeg",
+      comment:
+        "I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here",
+      role: "English teacher",
+      name: "Kalu Stephen Eke",
+    },
+    {
+      left: "/images/testimonial003.jpeg",
+      main: "/images/testimonial004.jpeg",
+      right: "/images/testimonial005.jpeg",
+      comment:
+        "The school provides a nurturing environment that fosters both academic and personal growth.",
+      role: "Parent",
+      name: "Joy Amadi",
+    },
+    {
+      left: "/images/testimonial004.jpeg",
+      main: "/images/testimonial005.jpeg",
+      right: "/images/testimonial006.jpeg",
+      comment:
+        "I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here.",
+      role: "Student",
+      name: "Peace Gentle",
+    },
+    {
+      left: "/images/testimonial005.jpeg",
+      main: "/images/testimonial006.jpeg",
+      right: "/images/testimonial001.jpeg",
+      comment:
+        "The school provides a nurturing environment that fosters both academic and personal growth.",
+      role: "Receptionist",
+      name: "Ada Precious",
+    },
+    {
+      left: "/images/testimonial006.jpeg",
+      main: "/images/testimonial001.jpeg",
+      right: "/images/testimonial002.jpg",
+      comment:
+        "I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here.",
+      role: "Head Boy",
+      name: "Emmanuel James",
+    },
   ];
 
-  const testimonials: string[] = [
-    "The teachers at this school are amazing! They truly care about each student's success. The teachers at this school are amazing! They truly care about each student's success. The teachers at this school are amazing! They truly care about each student's success.",
-    "I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here",
-    "The school provides a nurturing environment that fosters both academic and personal growth.",
-    "I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here.",
-    "The school provides a nurturing environment that fosters both academic and personal growth.",
-    "I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here. I love the friendly and inclusive atmosphere of this school. I feel safe and supported here.",
-  ];
+  const customPrevButton = (
+    <div className={styles.customPrevBtn}> &lsaquo;</div>
+  );
 
-  const [position, setPosition] = useState<imageIndices>({
-    first: 3,
-    second: 4,
-    third: 5,
-  });
-
-  const handleRight = () => {
-    if (position.first <= 0) {
-      setPosition({
-        first: 5,
-        second: 0,
-        third: 1,
-      });
-    } else if (position.second <= 0) {
-      setPosition({
-        first: 4,
-        second: 5,
-        third: 0,
-      });
-    } else if (position.third <= 0) {
-      setPosition({
-        first: 3,
-        second: 4,
-        third: 5,
-      });
-    } else {
-      setPosition((prev: imageIndices) => ({
-        first: prev.first === 0 ? 5 : prev.first - 1,
-        second: prev.second === 0 ? 5 : prev.second - 1,
-        third: prev.third === 0 ? 5 : prev.third - 1,
-      }));
-    }
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      handleRight();
-    }, 5000);
-    return () => {
-      clearInterval(interval);
-    };
-  });
-
-  // console.log(position);
-
+  const customNextButton = (
+    <div className={styles.customNextBtn}> &rsaquo;</div>
+  );
   return (
-    <section className={styles.container}>
-      <div className={styles.topDiv}>
-        <FaQuoteLeft className={styles.quoteIconLeft} size={36} color="#fff" />
-        <div className={styles.quoteBox}>
-          <p className={styles.quotesPara}> {testimonials[position.second]}</p>
-        </div>
-        <p className={styles.quoteAuthor}>Amarachi Ezenwaka</p>
-        <p className={styles.quoteRole}>Mathematics Teacher</p>
-      </div>
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={{
+          prevEl: `${styles.customPrevBtn}`,
+          nextEl: "styles.customNextBtn",
+        }}
+        modules={[Pagination, Navigation, Autoplay]}
+        className={styles.container}
+      >
+        {testimonials.map((testimonial: testimonial) => {
+          return (
+            <SwiperSlide>
+              <div className={styles.wrapper}>
+                <div className={styles.topDiv}>
+                  <FaQuoteLeft
+                    className={styles.quoteIconLeft}
+                    size={36}
+                    color="#fff"
+                  />
+                  <div className={styles.quoteBox}>
+                    <p className={styles.quotesPara}>{testimonial.comment}</p>
+                  </div>
+                  <p className={styles.quoteAuthor}>{testimonial.name}</p>
+                  <p className={styles.quoteRole}>{testimonial.role}</p>
+                </div>
 
-      <div className={styles.wrapper}>
-        <div className={styles.left}>
-          <div className={styles.imgWrapper}>
-            <Image
-              src={images[position.first]}
-              alt="recommendation"
-              fill={true}
-              className={styles.image}
-            />
-          </div>
-        </div>
-        <div className={styles.mid}>
-          <div className={styles.midImgWrapper}>
-            <Image
-              src={images[position.second]}
-              alt="recommendation"
-              fill={true}
-              className={styles.image}
-            />
-          </div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.imgWrapper}>
-            <Image
-              src={images[position.third]}
-              alt="recommendation"
-              fill={true}
-              className={styles.image}
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+                <div className={styles.commentWrapper}>
+                  <div className={styles.left}>
+                    <div className={styles.imgWrapper}>
+                      <Image
+                        src={testimonial.left}
+                        alt="recommendation"
+                        fill={true}
+                        className={styles.image}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.mid}>
+                    <div className={styles.midImgWrapper}>
+                      <Image
+                        src={testimonial.main}
+                        alt="recommendation"
+                        fill={true}
+                        className={styles.image}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.right}>
+                    <div className={styles.imgWrapper}>
+                      <Image
+                        src={testimonial.right}
+                        alt="recommendation"
+                        fill={true}
+                        className={styles.image}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
   );
 };
 
